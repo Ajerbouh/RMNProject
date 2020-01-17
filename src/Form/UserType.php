96e2272e4726d7c1cdf5fda11aed015549fff3f9
Ajerbouh <amine.jerb@gmail.com>
2020-01-17 11:27:39 +0100

    birthday datepicker works

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -3,18 +3,22 @@
 namespace App\Form;
 
 use App\Entity\User;
+use Doctrine\DBAL\Types\TextType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Security\Core\Role\Role;
 use Symfony\Component\Validator\Constraints\Choice;
+use Symfony\Component\Validator\Constraints\DateTime;
 
 class UserType extends AbstractType
 {
@@ -24,7 +28,9 @@ class UserType extends AbstractType
             ->add('firstname')
             ->add('lastname')
             ->add('email', EmailType::class)
-            ->add('birthday', BirthdayType::class)
+            ->add('birthday', \Symfony\Component\Form\Extension\Core\Type\TextType::class, [
+                'attr' => ['class' => 'datepicker'],
+            ])
             ->add('password', RepeatedType::class)
             ->add('password', RepeatedType::class, [
                 'type' => PasswordType::class,
